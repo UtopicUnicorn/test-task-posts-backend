1e1001984b8fbed6bae366337998fc1699377210
UtopicUnicorn <medvedevtimofei@gmail.com>
2023-03-07 23:35:13 +0700

    solved the problem with docker container termination

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ app.use('/', postRouter);
 app.listen(3000, () =>
   console.log(`Server running on http://localhost:${3000}`)
 );
+
+//process listeners to stop app
+process.on('SIGINT', function () {
+  process.exit();
+});
+
+process.on('SIGTERM', function () {
+  process.exit();
+});
